7913533d8ddba5af7cd63d12eb10bb5d33517589
Palle Westermann <pwe@tec.dk>
2026-02-04 13:29:51 +0100

    Next Up Is Signals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { AddEmployee } from './pages/add-employee/add-employee';
+import { DataBinding } from './pages/data-binding/data-binding';
+import { EmployeeList } from './pages/employee-list/employee-list';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path:'',
+    redirectTo:'data-binding',
+    pathMatch: 'full'
+  },
+  {
+    path:'add-employee',
+    component:AddEmployee,
+  },
+  {
+    path:'data-binding',
+    component:DataBinding,
+  },
+  {
+    path:'employee-list',
+    component:EmployeeList,
+  },
+  {
+    path:'**',
+    component:NotFound
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { AddEmployee } from "./pages/add-employee/add-employee";
 import { EmployeeList } from "./pages/employee-list/employee-list";
 import { DataBinding } from "./pages/data-binding/data-binding";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DataBinding],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
