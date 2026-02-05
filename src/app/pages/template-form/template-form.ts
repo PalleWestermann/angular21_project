40a5b1ed7502e8da62c1f3050a0b2b5f34865cc5
Palle Westermann <pwe@tec.dk>
2026-02-05 12:45:34 +0100

    Left Out The Forms Validation

--- src/app/pages/template-form/template-form.ts
+++ src/app/pages/template-form/template-form.ts
@@ -1,11 +1,21 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
-  imports: [],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './template-form.html',
   styleUrl: './template-form.css',
 })
 export class TemplateForm {
+
+  empObj = {
+    name: '',
+    role: '',
+    department: '',
+    salary: 0,
+    isActive: false
+  }
 
 }
